586432f601e56886c361d9ba271bd2f54cd79fd2
zzolain <zzolain@gmail.com>
2018-01-03 22:49:34 +0900

    Build Proto-type

--- FRONTEND/src/components/App/index.js
+++ FRONTEND/src/components/App/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import styles from "./styles.scss";
+import Asdf from '../asdf'
 
 class App extends Component {
   render() {
-    return <div className={styles.App} >
-    asdfadf</div>;
+    return (
+    <div className={styles.App} >
+      <Asdf />
+    </div>
+    )
   }
 }
 
