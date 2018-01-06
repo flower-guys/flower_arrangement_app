9b6b902860af78133e6f6c6a0706d7c41731f211
zzolain <zzolain@gmail.com>
2018-01-06 23:44:46 +0900

    Test toAdd & toRemove flowers

--- FRONTEND/src/components/FlowerImage/container.js
+++ FRONTEND/src/components/FlowerImage/container.js
@@ -4,11 +4,28 @@ import DB from './DB'
 
 export default class Container extends Component {
     state = {
-        selectedFlower : DB[0]
+        selectedFlowers : [DB[0], DB[0], DB[1]]
+    }
+    renderFlowers () {
+        return this.state.selectedFlowers.map( flower => {
+            return <FlowerImage selectedFlower={flower} key={flower.id} />
+        })
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                selectedFlowers: [DB[0], DB[1], DB[2]]
+            })
+        }, 5000);
+        setTimeout(() => {
+            this.setState({
+                selectedFlowers: [DB[1]]
+            })
+        }, 8000);
     }
     render() {
         return (
-                <FlowerImage selectedFlower={this.state.selectedFlower} />
+                this.renderFlowers()
         )
     }
 }
